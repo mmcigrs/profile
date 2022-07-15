856ba00eacea8834ceb24de414ea68b0ab5123f3
m-igarashi <107655755+mmcigrs@users.noreply.github.com>
2022-07-15 08:15:24 +0000

    PHP/Laravel05 kadai

---
+++ edit.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Controller/View</title>
+    </head>
+    <body>
+        <h1>ControllerとView連携</h1>
+        <h3>edit.blade.php</h3>
+        <br>
+        <p>Bladeファイルを設置しました</p>
+    </body>
+</html>
