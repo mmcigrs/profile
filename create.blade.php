cbd250e273ce05fe278016fb18ea5241ecb9275c
m-igarashi <107655755+mmcigrs@users.noreply.github.com>
2022-07-16 01:38:17 +0000

    PHP/Laravel06 kadai

--- create.blade.php
+++ create.blade.php
@@ -1,16 +1,13 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.profile')
 
-        <title>Controller/View</title>
-    </head>
-    <body>
-        <h1>ControllerとView連携</h1>
-        <h3>create.blade.php</h3>
-        <br>
-        <p>Bladeファイルを設置しました</p>
-    </body>
-</html>
+@section('title','プロフィールの作成')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>Myプロフィール</h2>
+            </div>
+        </div>
+    </div>
+@endsection
